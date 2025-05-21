df6a1b9aaba66a2194565b3f1516b67a01cacae5
Angelo Rajendram <aa3rajen@beluga2.int.ets1.calculquebec.ca>
2025-05-21 18:36:50 -0400

    Use m23 instead of m13

--- config_management/data_config_muon_decay.py
+++ config_management/data_config_muon_decay.py
@@ -2,7 +2,7 @@
 class DataConfig:
     data_file_path = "./data/MuonDecay.csv"
     processed_data_prefix = "muon_decay"
-    columns = ['m12^2', 'm13^2']
+    columns = ['m12^2', 'm23^2']
     b_adj = 1e-2 # Bandwidth adjustment for KDE adjustment factor on scott's factor for the bandwidth
     K = 'gaussian' # Kernel type for KDE
     slice_num = 8 # Number of slices for conditional distributions
