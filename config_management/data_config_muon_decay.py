117aa5f8e3375321babbebf72eb92eaffba255e3
angelo <52296518+Angelo92Git@users.noreply.github.com>
2025-05-21 21:28:53 -0400

    Update data generation for muon_decay

--- config_management/data_config_muon_decay.py
+++ config_management/data_config_muon_decay.py
@@ -3,6 +3,6 @@ class DataConfig:
     data_file_path = "./data/MuonDecay.csv"
     processed_data_prefix = "muon_decay"
     columns = ['m12^2', 'm23^2']
-    b_adj = 0.002 # Bandwidth adjustment for KDE adjustment factor on scott's factor for the bandwidth
+    b_adj = 1e-2 # Bandwidth adjustment for KDE adjustment factor on scott's factor for the bandwidth
     K = 'tophat' # Kernel type for KDE
-    slice_num = 8 # Number of slices for conditional distributions
+    slice_num = 15 # Number of slices for conditional distributions
--- gen_data.py
+++ gen_data.py
@@ -5,6 +5,8 @@ import pandas as pd
 from tqdm import tqdm
 from config_management.data_config_muon_decay import DataConfig
 
+seed = 42
+
 def kde1D(x, bandwidth, xbins=100j, **kwargs):
     """Build 1D kernel density estimate (KDE)."""
     X = np.mgrid[x.min():x.max():xbins]
@@ -34,7 +36,7 @@ def generate_conditionals(samples, save_prefix, K = 'gaussian', slice_num=8, b_a
     # conditional_slices_[0-1].csv
     print("generating conditionals...")
 
-    bw = b_adj * scipy.stats.gaussian_kde(samples[:, 0]).scotts_factor()
+    bw = b_adj * scipy.stats.gaussian_kde(samples.T).scotts_factor()
     kde_all = KernelDensity(bandwidth=bw, kernel=K)
     kde_all.fit(samples)
     var_num = samples.shape[1]
@@ -44,7 +46,9 @@ def generate_conditionals(samples, save_prefix, K = 'gaussian', slice_num=8, b_a
         for i in range(var_num - 1):
             data[:, i] = samples[:, i + (i >= vid)]
         kde_x_other.fit(data)
-        x_other_sample = kde_x_other.sample(slice_num)
+        x_other_sample = kde_x_other.sample(slice_num, random_state=seed)
+        if x_other_sample.shape[1] == 1:
+            x_other_sample = np.sort(x_other_sample, axis=0)
         x_other_prob = np.exp(kde_x_other.score_samples(x_other_sample))
         for snum in tqdm(range(slice_num), desc=f"for x{vid}: ", leave=False):
             x_vid = np.mgrid[min(samples[:, vid]):max(samples[:, vid]):100j]
@@ -52,7 +56,7 @@ def generate_conditionals(samples, save_prefix, K = 'gaussian', slice_num=8, b_a
             x_all[:, vid] = x_vid
             for k in range(var_num - 1):
                 cur_id = k + (k >= vid)
-                x_all[:, cur_id] = x_other_sample[i][k]
+                x_all[:, cur_id] = x_other_sample[snum][k]
             prob = np.exp(kde_all.score_samples(x_all)) # We use kde_all to score the slice of joint distribution corresponding to (x1, x2, x3)
             normalize_coe = len(x_all) / np.sum(prob) / (np.max(x_vid) - np.min(x_vid)) # This is the normalizing coefficient (area under the curve of the slice)
             prob *= normalize_coe
@@ -67,8 +71,8 @@ def generate_conditionals(samples, save_prefix, K = 'gaussian', slice_num=8, b_a
 def main():
     samples = read_file(DataConfig.data_file_path, DataConfig.columns)
     num_var = len(DataConfig.columns)
-    generate_marginals(samples, num_var, save_prefix = DataConfig.processed_data_prefix, b_adj=DataConfig.b_adj)
-    generate_conditionals(samples, save_prefix = DataConfig.processed_data_prefix, K=DataConfig.K, slice_num=DataConfig.slice_num, b_adj=DataConfig.b_adj)
+    generate_marginals(samples, num_var, save_prefix = DataConfig.processed_data_prefix, b_adj=DataConfig.b_adj*1.0)
+    generate_conditionals(samples, save_prefix = DataConfig.processed_data_prefix, K=DataConfig.K, slice_num=DataConfig.slice_num, b_adj=DataConfig.b_adj*1.3)
     print("done!")
 
 if __name__ == "__main__":
